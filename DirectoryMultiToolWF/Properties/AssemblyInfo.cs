74a1cc54f457cfbf8f9b7f380fa6b2b3f0ca021e
Fox-Alpha <fox-alpha@tarnhoerner.de>
2016-07-08 22:28:40 +0200

    Add: Initiales Projekt mit einigen Grundeinstellungen

---
+++ DirectoryMultiToolWF/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle ("DirectoryMultiToolWF")]
+[assembly: AssemblyDescription ("Einfaches anlegen von Verzeichnisstrukturen")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Buck-Software")]
+[assembly: AssemblyProduct ("DirectoryMultiToolWF")]
+[assembly: AssemblyCopyright ("Copyright © Dirk Buck 2016")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible (false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid ("8e427a4c-8703-4e1c-9ee6-593210d56cd7")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion ("1.0.0.0")]
+[assembly: AssemblyFileVersion ("1.0.0.0")]
