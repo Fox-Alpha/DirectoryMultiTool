b5a23ef2d4b73e38729d7d9025b53d6e1e0797e7
Fox-Alpha <fox-alpha@tarnhoerner.de>
2016-07-08 23:31:21 +0200

    Add: Definieren der Klasse für die Konfigurationsdatei

--- DirectoryMultiToolWF/Form1.cs
+++ DirectoryMultiToolWF/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
@@ -18,6 +19,45 @@ namespace DirectoryMultiToolWF
 		public Form1 ()
 		{
 			InitializeComponent ();
+		}
+	}
+
+	/* Klasse zum einlesen der JSON Konfiguration */
+	[JsonObject (MemberSerialization.OptIn)]
+	public class DirectoryTask
+	{
+		/* Die Datei die als Konfiguration geladen werden soll */
+		string jsonFile	{ get; set;	}
+
+		[JsonProperty(PropertyName ="Names", Required = Required.Always)]
+		List<string> targetNames;
+		[JsonProperty(PropertyName = "Seperator", DefaultValueHandling = DefaultValueHandling.Populate)]
+		List<string> nameSeperator;
+		[JsonProperty(Required = Required.AllowNull)]
+		Dictionary<string, string> nameAliases;
+		[JsonProperty(Required = Required.Always)]
+		string rootDirectory { get; set; }
+		[JsonProperty(Required = Required.Default)]
+		bool silentTask 
+		{ get; set; }
+		[JsonProperty(Required = Required.Default)]
+		string logFile
+		{ get; set; }
+		// Templates
+		[JsonProperty (Required = Required.Default)]
+		string tplDirectory
+		{ get; set; }
+		[JsonProperty(Required = Required.Default)]
+		List<string> tplFilter;
+		[JsonProperty(Required = Required.Default)]
+		List<string> tplFiles;
+
+		public DirectoryTask(string _jsonFile)
+		{
+			if (File.Exists(jsonFile))
+			{
+				//	Einlesen der Konfiguration
+			}
 		}
 	}
 }
